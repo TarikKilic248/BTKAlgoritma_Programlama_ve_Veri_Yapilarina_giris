7c7627e060d9fe8e23929e18391938967d6e3a9d
Tarık Kılıç Alsancak <95878587+TarikKilic248@users.noreply.github.com>
2022-09-03 14:23:19 +0000

    Class Tanımı yapıldı

--- main.cs
+++ main.cs
@@ -1,7 +1,36 @@
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World");
+
+        OgretimElemani ogrGor = new OgretimElemani();
+        Console.WriteLine(ogrGor);
+    }
+
+    public class OgretimElemani
+    {
+        //Property - Özellik
+        public int SicilNo { get; set; }
+        public string Adi { get; set; }
+        public string Soyadi { get; set; }
+        public bool Cinsiyet { get; set; }
+
+        //Default Constructor
+        public OgretimElemani()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return $"{SicilNo,-5}" +
+              $"{Adi,-10}" +
+              $"{Soyadi,-10}" +
+              $"{Cinsiyet,-10}";
+        }
+    }
 }
+
