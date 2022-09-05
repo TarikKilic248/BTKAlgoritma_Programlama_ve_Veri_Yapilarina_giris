b5d8ee1d33b35db8ce2831e0a6f7c86f9deee9ac
Tarık Kılıç Alsancak <95878587+TarikKilic248@users.noreply.github.com>
2022-09-05 14:32:44 +0000

    Array Class Metodları yazıldı

--- main.cs
+++ main.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 
 public class Program
 {
@@ -7,22 +8,50 @@ public class Program
         Console.WriteLine("Hello World");
 
         //Array Dizi tanımı
-        int[] sayilar = new int[] { 1, 3, 5, 7, 9 };
+        int[] sayilar = new int[] { 5, 3, 11, 9, 7, 13, 17, 1, 15 };
         //Alternatif
-        var sayilar2 = Array.CreateInstance(typeof(int), 5);
+        var sayilar2 = Array.CreateInstance(typeof(int), sayilar.Length);
+
+        /*  
         sayilar2.SetValue(0, 0);
         sayilar2.SetValue(2, 1);
         sayilar2.SetValue(4, 2);
         sayilar2.SetValue(6, 3);
         sayilar2.SetValue(8, 4);
+        */
 
-        //Dolaşma
+        /*
+          for (int i = 0; i <  sayilar.Length; i++)
+          {
+              sayilar2.SetValue(sayilar[i], i);
+          }
+        */
+
+        sayilar.CopyTo(sayilar2, 0);
+
+        //Alternatif
+        var sayilar3 = new ArrayList(sayilar);
+
+        //Sıralama
+        Array.Sort(sayilar);
+        Array.Sort(sayilar2);
+        sayilar3.Sort();
+
+        //Silme
+        Array.Clear(sayilar, 2, 2);
+
+        //Bulma
+        Console.WriteLine(Array.IndexOf(sayilar, 1));
+
+
+        //Array Dizi Dolaşma
         for (int i = 0; i < sayilar2.Length; i++)
         {
-            Console.WriteLine("sayilar[{0}] = {1} - sayilar2[{0}] = {2}",
-                     i, sayilar[i], sayilar2.GetValue(i));
-            //Console.WriteLine($"sayilar[{i}] = " + $"{sayilar[i]} - " +
-            //         $"sayilar2[{i}] = {sayilar2.GetValue(i)}");
+            //Console.WriteLine("sayilar[{0}] = {1} - sayilar2[{0}] = {2}",
+            //         i, sayilar[i], sayilar2.GetValue(i));
+            Console.WriteLine($"sayilar[{i}] = " + $"{sayilar[i],3}  -  " +
+                       $"sayilar2[{i}] = {sayilar2.GetValue(i),3}  -  " +
+                       $"sayilar3[{i}] = " + $"{sayilar3[i],3}");
         }
 
     }
