deb466a8ecb922de958b976fa8600b101a977d7d
Tarık Kılıç Alsancak <95878587+TarikKilic248@users.noreply.github.com>
2022-09-06 18:21:06 +0000

    Hashtable uygulaması yapıldı girilen string ifadenin uygun harflere çevrilmesi programlardı

--- main.cs
+++ main.cs
@@ -5,53 +5,35 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello World\n");
+        //Hashtable uygulamasi
 
-        //Hashtable tanımlama
-        var sehirler = new Hashtable();
+        //Başlığı okuma
+        Console.WriteLine("Başlık giriniz: ");
+        string baslik = Console.ReadLine();
 
-        //Ekleme
-        sehirler.Add(6, "Ankara");
-        sehirler.Add(34, "İstanbul");
-        sehirler.Add(55, "Samsun");
-        sehirler.Add(23, "Elazığ");
+        //Küçültme
+        baslik = baslik.ToLower();
 
-        //Dolaşma
-        foreach (DictionaryEntry item in sehirler)
-        {
-            Console.WriteLine($"{item.Key,-5}" + $" - {item.Value,-20}");
-        }
+        //Hashtable
+        var karakterSeti = new Hashtable()
+      {
+        {'ç','c'},
+        {'ı','i'},
+        {'ö','o'},
+        {'ü','u'},
+        {' ','-'},
+        {'\'','-'},
+        {'ğ','g'},
+        {'.','-'}
+      };
 
-        //Anahtarları alma
-        Console.WriteLine("\nAnahtarlar (keys)");
-        var anahtarlar = sehirler.Keys;
-        foreach (var item in anahtarlar)
-        {
-            Console.WriteLine(item);
-        }
+      foreach(DictionaryEntry item in karakterSeti)
+      {
+        baslik = baslik.Replace((char)item.Key, (char)item.Value);
+      }
 
-        //Değerler
-        Console.WriteLine("\nDeğerler (Values)");
-        ICollection degerler = sehirler.Values;
-
-        foreach (var item in degerler)
-        {
-            Console.WriteLine(item);
-        }
-
-        //Elemana erişme
-        Console.WriteLine("\nElemana erişme");
-        Console.WriteLine(sehirler[55]);
-
-        //Eleman silme
-        Console.WriteLine("Eleman silme\n");
-        sehirler.Remove(23);
-
-        foreach (DictionaryEntry item in sehirler)
-        {
-            Console.WriteLine($"{item.Key,-5}" + $" - {item.Value,-20}");
-        }
-
+      //Ekrana yazdirma
+      Console.WriteLine(baslik);
     }
 }
 
