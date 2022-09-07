64ee8cd42f3faf6970f4f0704df6abc85322df7e
Tarık Kılıç Alsancak <95878587+TarikKilic248@users.noreply.github.com>
2022-09-07 13:10:22 +0000

    SortedList Uygulaması Yapıldı

--- main.cs
+++ main.cs
@@ -5,70 +5,21 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        //SortedList - Temmeller
-        //Tanımlama
-        var list = new SortedList()
+        //SortedList
+        var kitapIcerigi = new SortedList();
+        kitapIcerigi.Add(1, "Önsöz");
+        kitapIcerigi.Add(50, "Değişkenler");
+        kitapIcerigi.Add(40, "Operatörler");
+        kitapIcerigi.Add(60, "Döngüler");
+        kitapIcerigi.Add(45, "İlişkisel Operatörler");
+
+        Console.WriteLine("İçindekiler");
+        Console.WriteLine(new string('-', 25));
+        Console.WriteLine($"{"Sayfalar",8} {"Konular",-33}");
+        foreach (DictionaryEntry item in kitapIcerigi)
         {
-          {1,"Bir"},
-          {2,"İki"},
-          {3,"Üç"},
-          {8,"Sekiz"},
-          {5,"Beş"},
-          {6,"Altı"},
-        };
-
-        list.Add(4, "Dört");
-        list.Add(7, "Yedi");
-
-
-        //Dolaşma
-        foreach (DictionaryEntry item in list)
-        {
-            Console.WriteLine($"{item.Key} - {item.Value}");
+            Console.WriteLine($"{item.Key,5} {item.Value,-33}");
         }
-
-        Console.WriteLine("\nListedeki eleman sayısı : {0}", list.Count);
-        Console.WriteLine("Listenin kapasitesi     : {0}", list.Capacity);
-        list.TrimToSize();
-        Console.WriteLine("Listenin kapasitesi     : {0}\n", list.Capacity);
-
-        //Erişme
-        //Key'e bağlı olarak
-        Console.WriteLine(list[4]);
-        //İndex'e bağlı olarak
-        Console.WriteLine(list.GetByIndex(0));
-        //Get -> Key
-        Console.WriteLine(list.GetKey(0));
-        //Liste sonundaki elemanın değeri
-        Console.WriteLine(list.GetByIndex(list.Count - 1));
-
-        var anahtarlar = list.Keys;
-        Console.WriteLine("\nAnahtarlar:");
-        foreach (var item in anahtarlar)
-        {
-            Console.WriteLine(item);
-        }
-
-        var degerler = list.Values;
-        Console.WriteLine("\nDeğerler:");
-        foreach (var item in degerler)
-        {
-            Console.WriteLine(item);
-        }
-
-        //Güncelleme
-        if (list.ContainsKey(1))
-        {
-            list[1] = "One";
-        }
-        //Yeni Dolaşma 
-        foreach (DictionaryEntry item in list)
-        {
-            Console.WriteLine($"{item.Key} - {item.Value}");
-        }
-
-
-
     }
 }
 
