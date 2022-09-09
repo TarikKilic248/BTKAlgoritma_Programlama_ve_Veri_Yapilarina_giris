83241aa80274b71ad1f695fd5571caa666fe04a8
Tarık Kılıç Alsancak <95878587+TarikKilic248@users.noreply.github.com>
2022-09-09 14:11:28 +0000

    Liste ve İnterface(arayüz) konusunu ele aldık

--- main.cs
+++ main.cs
@@ -1,25 +1,28 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Program
 {
+    /*
+    public class Araba<T> : IEnumerable<T> // : kalıtımın simgesidir
+    {
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    */
+
+
     public static void Main(string[] args)
     {
-        //SortedList
-        var kitapIcerigi = new SortedList();
-        kitapIcerigi.Add(1, "Önsöz");
-        kitapIcerigi.Add(50, "Değişkenler");
-        kitapIcerigi.Add(40, "Operatörler");
-        kitapIcerigi.Add(60, "Döngüler");
-        kitapIcerigi.Add(45, "İlişkisel Operatörler");
-
-        Console.WriteLine("İçindekiler");
-        Console.WriteLine(new string('-', 25));
-        Console.WriteLine($"{"Sayfalar",8} {"Konular",-33}");
-        foreach (DictionaryEntry item in kitapIcerigi)
-        {
-            Console.WriteLine($"{item.Key,5} {item.Value,-33}");
-        }
+      var list = new List<int>();
     }
 }
 
